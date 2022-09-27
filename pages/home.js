e1c762907f14f8290beb24c127d561ee2f6b9921
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-27 17:42:31 +0600

    adding hompage login system

--- pages/home.js
+++ pages/home.js
@@ -21,9 +21,22 @@ function Home() {
                         <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
                         <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
                     </div>
+                    <div className="pl-4">
+                        <button className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2">Sign in</button>
+                    </div>
+                </div>
+            </header>
+
+            <main className="flex flex-col xl:flex-row  max-w-screen-lg  mx-auto">
+                <div className="space-y-6 xl:space-y-10">
+                    <h1 className="text-3xl md:text-5xl max-w-xl !leading-snug text-amber-800/80">Welcome to our professional community</h1>
+
                 </div>
 
-            </header>
+            </main>
+
+
+
         </div>
     )
 }
