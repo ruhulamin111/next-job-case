319de92e9ccf816626351e13843a8b43efb5e7e8
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-30 08:31:10 +0600

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,25 +1,22 @@
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import clientPromise from "../../../lib/mongodb"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
 
-export const authOptions = {
+export default NextAuth({
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
-        // ...add more providers here
     ],
-    secret: process.env.JWT.SECRET,
+    secret: process.env.JWT_SECRET,
     adapter: MongoDBAdapter(clientPromise),
     pages: {
-        signIn: '/home'
+        signIn: "/home",
     },
-    season: {
-        stratregy: 'jwt'
+    session: {
+        strategy: "jwt",
     },
-}
-
-export default NextAuth(authOptions)
+});
