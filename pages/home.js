13653ba24bb696c52939f312afa48cca24b85375
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-30 08:01:12 +0600

    user create done

--- pages/home.js
+++ pages/home.js
@@ -7,8 +7,9 @@ import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import search from '../images/search.jpg';
 import Head from "next/head";
+import { getProviders, signIn } from "next-auth/react";
 
-function Home() {
+function Home({ providers }) {
 
     return (
         <div className="space-y-10">
@@ -29,9 +30,24 @@ function Home() {
                         <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
                         <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
                     </div>
-                    <div className="pl-4">
+
+                    {Object.values(providers).map((provider) => (
+                        <div key={provider.name}>
+                            <div className="pl-4">
+                                <button
+                                    className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2"
+                                    onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                                >
+                                    Sign in
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+
+                    {/* <div className="pl-4">
                         <button className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2">Sign in</button>
-                    </div>
+                    </div> */}
+
                 </div>
             </header>
 
@@ -58,11 +74,20 @@ function Home() {
                 </div>
 
 
-
-
             </main>
         </div>
     )
 }
 
 export default Home
+
+
+export async function getServerSideProps(context) {
+    const providers = await getProviders();
+
+    return {
+        props: {
+            providers,
+        },
+    };
+}
